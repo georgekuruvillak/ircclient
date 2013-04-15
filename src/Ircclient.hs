6d7e507e14c6ad15ac412ed8bfcae0a1039d407f
George <georgekuruvillak@gmail.com>
2013-04-16 00:56:00 +0530

    password fn implemented

--- src/Ircclient.hs
+++ src/Ircclient.hs
@@ -32,3 +32,83 @@ isTilde::String->Bool
 isTilde (x:xs)     | (x =='~') = True
 				   | otherwise = False
 
+{- getCommand seperates the command from the user input-}
+				   
+getCommand::String -> String
+			
+getCommand str = head $ words (removeTilde str)
+
+{- getParams seperates the parameters from the whole user input -}
+
+getParams::String -> String
+
+getParams str = drop (length $ getCommand (removeTilde str)) (removeTilde str)
+
+{-parseCmd' parses the user command and decides the type-}
+				
+parseCmd'::String->String
+
+parseCmd' str=	case (isTilde str) of
+									True  -> do 
+												case (isTilde (removeTilde str)) of 
+																					True -> parseCmd''' str
+																					False -> parseCmd'' str
+													
+									False -> "PRIVMSG " ++ annotateAsMsg str
+
+{-parseCmd'' is the helper function of parseCmd' -}
+
+parseCmd'' :: String -> String
+
+parseCmd'' str = (map toUpper (getCommand str)) ++ (getParams str)													
+
+{-parseCmd''' is the helper function of parseCmd'' -}
+
+parseCmd'''::String->String
+											
+parseCmd''' str = "NickServ " ++ (parseCmd'' (removeTilde str))
+
+annotateAsMsg :: String->String	
+
+annotateAsMsg s=unwords((head(words s)):((annotateHelper s):(rest s)))
+
+annotateHelper :: String->String
+
+annotateHelper s=(':':((words s)!!1))
+
+rest::String->[String]
+
+rest s= tail $ tail $ words s
+
+{-listen listens the messages from -}
+
+listen :: Handle -> IO ()
+
+listen h = forever $ do
+				s <- hGetLine h
+				printf    "> %s \n" s				 
+
+{-write writes on the socket-}
+
+write :: Handle -> String -> IO ()
+
+write h s = do
+				hPrintf h "%s \n" s
+				print s
+				
+printStr str = do
+				putStr str
+				hFlush stdout
+
+{-getPassword hides user password-}
+
+getPassword :: IO String
+getPassword = do
+				  --putStr "Password: "
+				  --hFlush stdout
+				  pass <- withEcho False getLine
+				  putChar '\n'
+				  return pass
+
+
+
